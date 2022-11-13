71f8933245ca96b40bfd194496da30353ac2f06c
LeoZion <leozionsuporte@gmail.com>
2022-11-13 01:58:46 -0300

    refactor get data labels to use axios

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
+import { githubApi } from '../../api/githubApi'
+import { Label } from '../../interfaces/label'
 
-const getLabels = async () => {
-  const res = await fetch('https://api.github.com/repos/facebook/react/labels')
-  const data = await res.json()
+const getLabels = async ():Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels')
 
-  console.log(data)
   return data
 }
 
