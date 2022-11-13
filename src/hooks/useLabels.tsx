23861512c349f0e6d859700f57a03662165e5484
LeoZion <leozionsuporte@gmail.com>
2022-11-13 02:42:46 -0300

    initialData and placeholderData

--- src/hooks/useLabels.tsx
+++ src/hooks/useLabels.tsx
@@ -17,6 +17,34 @@ export const useLabels = () => {
     getLabels,
     {
       staleTime: 1000 * 60 * 60,
+      // initialData: [],
+      // placeholderData: [],
+      placeholderData: [
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue",
+          color: "6ce26a",
+          default: true,
+        },
+        {
+          id: 52079258,
+          node_id: "MDU6TGFiZWw1MjA3OTI1OA==",
+          url: "https://api.github.com/repos/facebook/react/labels/Difficulty:%20starter",
+          name: "Difficulty: starter",
+          color: "94ce52",
+          default: false,
+        },
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken)",
+          color: "b60205",
+          default: false,
+        }
+      ]
     }
   )
 
