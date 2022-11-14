5227b74d3df45c11a27ba9da80c5c125368879c8
LeoZion <leozionsuporte@gmail.com>
2022-11-14 11:37:02 -0300

    render issues data

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useIssues } from '../../hooks';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
@@ -17,7 +18,10 @@ export const ListView = () => {
   return (
     <div className="row mt-5">      
       <div className="col-8">
-        <IssueList />
+        {issuesQuery.isLoading
+          ? ( <LoadingIcon /> )
+          : ( <IssueList issues={ issuesQuery.data || [] } /> )
+        }
       </div>
       
       <div className="col-4">
